06231d6d81da4b5728d16e40dbef9b6c4eb90d0c
dimmerz92 <andrew.weymes@outlook.com>
2023-05-28 17:17:04 +0800

    add handbrake functionality

--- rip-disc.py
+++ rip-disc.py
@@ -1,19 +1,24 @@
 import subprocess
 import sys
+import os
 
-def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-
-    if process.returncode == 0:
-        print(output.decode("utf-8"))
+def run_command(command, wait=True, detach=False):
+    if detach:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, start_new_session=True)
     else:
-        print(error.decode)
-        sys.exit(-1)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+    if wait:
+        output, error = process.communicate()
+
+        if process.returncode == 0:
+            print(output.decode("utf-8"))
+        else:
+            print(error.decode("utf-8"))
+            sys.exit(-1)
 
 # LIST ALL DVD TITLES
-titlesCmd= "makemkvcon -r info disc:0 | grep MSG:3028 | awk -F ',' '{print $4, $5}'"
+titlesCmd= "makemkvcon -r info disc:0 | grep MSG:3028 | awk -F ',' 'BEGIN {printf \"%-9s %-50s\n\", \"Title ID\", \"Information\"} {printf \"%-9d %-50s\n\", NR-1, $4 \" \" $5}'"
 print("Getting titles from DVD\n")
 run_command(titlesCmd)
 
@@ -21,7 +26,8 @@ run_command(titlesCmd)
 # USER INPUT TO GET SPECIFIC TITLES
 titles = input("Enter titles to rip:\n").lower()
 path = input("Enter filepath to save output to:\n")
-ripCmd = "makemkvcon -r mkv disc:0 {} {}"
+ripCmd = "makemkvcon -r mkv disc:0 {} {} > /dev/null"
+convertCmd = "HandBrakeCLI -Z \"Fast 1080p30\" -i {} -o output/{}"
 
 if titles == "all":
     # rip all
@@ -45,5 +51,14 @@ elif "-" in titles:
 if titles == "all":
     run_command(ripCmd)
 else:
+    os.makedirs("output", exist_ok=True)
     for title in titles:
+        print(f"Title {title} being ripped\n")
         run_command(ripCmd.format(title, path))
+        files = os.listdir()
+        files = [file for file in files if file != ".DS_Store"]
+        files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+        run_command(convertCmd.format(files[0], files[0]), False, True)
+        print(f"Title {title} being compressed\n")
+
+run_command("drutil eject", False)
